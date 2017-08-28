f358332daf58aa75ed65febc70b90ec86214cd30
jingxiaohu <251878350@qq.com>
2017-08-28 18:11:00 +0800

    修改空白

--- src/main/java/com/pyb/jsoup/article/Nfcj_singleStock_Spider.java
+++ src/main/java/com/pyb/jsoup/article/Nfcj_singleStock_Spider.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
