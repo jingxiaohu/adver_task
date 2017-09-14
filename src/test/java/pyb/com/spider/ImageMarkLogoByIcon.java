00866bf558dc7b6cf631a6c1bffdcec4168bb4fb
jingxiaohu <251878350@qq.com>
2017-09-14 17:00:45 +0800

    修改空白

---
+++ src/test/java/pyb/com/spider/ImageMarkLogoByIcon.java
@@ -0,0 +1,113 @@
+package pyb.com.spider;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+/**
+ * 给图片添加水印
+ */
+public class ImageMarkLogoByIcon {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String basepath = "D:/temp/face_img/";
+        String iconPath = basepath+"logo.png";
+        String srcImgPath = basepath+"11111.jpg";
+        String targerPath = basepath+"11111_sy.jpg";
+        // 给图片添加水印
+        ImageMarkLogoByIcon.markImageByIcon(iconPath, srcImgPath, targerPath,1.0f);
+        // 给图片添加水印,水印旋转-45
+        //ImageMarkLogoByIcon.markImageByIcon(iconPath, srcImgPath, targerPath,-45);
+
+    }
+
+    /**
+     * 给图片添加水印
+     *
+     * @param iconPath   水印图片路径
+     * @param srcImgPath 源图片路径
+     * @param targerPath 目标图片路径
+     */
+    public static void markImageByIcon(String iconPath, String srcImgPath,
+                                       String targerPath,float alpha) {
+        markImageByIcon(iconPath, srcImgPath, targerPath,null,alpha);
+    }
+
+    /**
+     * 给图片添加水印、可设置水印图片旋转角度
+     *
+     * @param iconPath   水印图片路径
+     * @param srcImgPath 源图片路径
+     * @param targerPath 目标图片路径
+     * @param degree     水印图片旋转角度
+     */
+    public static void markImageByIcon(String iconPath, String srcImgPath,
+                                       String targerPath, Integer degree,float alpha) {
+        OutputStream os = null;
+        try {
+            Image srcImg = ImageIO.read(new File(srcImgPath));
+
+            BufferedImage buffImg = new BufferedImage(srcImg.getWidth(null),
+                    srcImg.getHeight(null), BufferedImage.TYPE_INT_RGB);
+
+            // 得到画笔对象
+            // Graphics g= buffImg.getGraphics();
+            Graphics2D g = buffImg.createGraphics();
+
+            // 设置对线段的锯齿状边缘处理
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                    RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+            g.drawImage(srcImg.getScaledInstance(srcImg.getWidth(null), srcImg.getHeight(null), Image.SCALE_SMOOTH), 0, 0, null);
+
+            if (null != degree) {
+                // 设置水印旋转
+                g.rotate(Math.toRadians(degree),(double) buffImg.getWidth() / 2, (double) buffImg.getHeight() / 2);
+            }
+
+            // 水印图象的路径 水印一般为gif或者png的，这样可设置透明度
+            ImageIcon imgIcon = new ImageIcon(iconPath);
+
+            // 得到Image对象。
+            Image img = imgIcon.getImage();
+
+            //float alpha = 0.5f; // 透明度
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, alpha));
+
+            // 表示水印图片的位置
+            if(imgIcon.getIconWidth() > buffImg.getWidth()/2){
+                g.drawImage(img, 0, 0,buffImg.getWidth()/2,buffImg.getHeight()/2, null);
+            }else{
+                g.drawImage(img, buffImg.getWidth()/2, buffImg.getHeight()/2, null);
+            }
+
+
+            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
+
+            g.dispose();
+
+            os = new FileOutputStream(targerPath);
+
+            // 生成图片
+            ImageIO.write(buffImg, "JPG", os);
+
+            System.out.println("图片完成添加Icon印章。。。。。。");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != os)
+                    os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
