b35f6cb8f7fbac58f771ad372d6eff9914c833b3
jingxiaohu <251878350@qq.com>
2017-12-08 17:18:46 +0800

    修改用户表字段添加微信号码

---
+++ src/main/java/com/pyb/mvc/action/v1/weixin/user/Write_applyUserAction.java
@@ -0,0 +1,87 @@
+
+package com.pyb.mvc.action.v1.weixin.user;
+
+import com.pyb.bean.ReturnDataNew;
+import com.pyb.constants.Constants;
+import com.pyb.mvc.action.v1.BaseV1Controller;
+import com.pyb.mvc.action.v1.weixin.user.param.Param_userinfo;
+import com.pyb.mvc.weixin.biz.UserManageBiz;
+import com.pyb.util.RequestUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 申请成为推荐合作人
+ *
+ * @author jingxiaohu
+ */
+@RestController
+@RequestMapping(value = "/v1")
+public class Write_applyUserAction extends BaseV1Controller {
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = 6891425545908564737L;
+
+  @Autowired
+  UserManageBiz userManageBiz;
+
+  /**
+   * 申请成为推荐合作人
+   */
+  @RequestMapping(value = "/goods/apply_user")
+  @ResponseBody
+  public String apply_user(HttpServletRequest request, HttpServletResponse response, Param_userinfo param) {
+
+
+    ReturnDataNew returnData = new ReturnDataNew();
+    try {
+      //参数检查
+      if (param == null) {
+        //参数传递错误
+        returnData.setReturnData(errorcode_param, "参数传递错误", "");
+        sendResp(returnData, response);
+        return null;
+      }
+      //检查是否进行了参数签名认证
+      if (!param.checkRequest()) {
+        returnData.setReturnData(errorcode_param, "没有进行参数签名认证", "");
+        sendResp(returnData, response);
+        return null;
+      }
+      //对封装的参数对象中的属性进行 非空等规则验证
+      if (RequestUtil.checkObjectBlank(param.sign)) {
+        returnData.setReturnData(errorcode_param, " sign is null", "");
+        sendResp(returnData, response);
+        return null;
+      }
+      String sign_str = getSignature(Constants.getSystemKey(param.dtype), param.ui_id);
+      if (!param.sign.equalsIgnoreCase(sign_str)) {
+        log.warn("sign=" + param.sign + "  sign_str=" + sign_str);
+        returnData.setReturnData(errorcode_param, " sign is not right", null);
+        sendResp(returnData, response);
+        return null;
+      }
+
+      userManageBiz.GainUserInfo(returnData,param);
+      sendResp(returnData, response);
+      return null;
+
+    } catch (Exception e) {
+      log.error("Read_userInfoAction.user_info is error  获取用户个人信息 - P", e);
+      returnData.setReturnData(errorcode_systerm, "system is error", "");
+    }
+    sendResp(returnData, response);
+    return null;
+  }
+
+
+
+
+}
--- src/main/java/com/pyb/mvc/weixin/biz/UserManageBiz.java
+++ src/main/java/com/pyb/mvc/weixin/biz/UserManageBiz.java
@@ -1,6 +1,7 @@
 package com.pyb.mvc.weixin.biz;
 
 import com.pyb.bean.ReturnDataNew;
+import com.pyb.bean.Wx_recommend_earnings;
 import com.pyb.bean.Wx_user_info;
 import com.pyb.mvc.action.v1.weixin.user.param.Param_userinfo;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,48 @@ public class UserManageBiz extends  BaseWxBiz{
             returnData.setReturnData(errorcode_systerm, "GoodsBiz GainUserInfo is error", "");
         }
     }
+
+    /**
+     * 申请成为推荐合作人
+     */
+    public void ApplyUser(ReturnDataNew returnData, Param_userinfo param) {
+        try {
+            String sql = "select * from wx_recommend_earnings where ui_id=? limit 1";
+            Wx_recommend_earnings wx_recommend_earnings = getDB().queryUniqueT(sql,Wx_recommend_earnings.class,param.getUi_id());
+            if(wx_recommend_earnings != null){
+                returnData.setReturnData(errorcode_data, "已经是合作人了", "","1");
+                return;
+            }
+            //推荐合伙人记录没有  构建新的申请记录
+            wx_recommend_earnings = new Wx_recommend_earnings();
+            Wx_user_info userinfo = daoFactory.getWx_user_infoDao().selectByKey(param.getUi_id());
+            wx_recommend_earnings.setUi_id(userinfo.getUi_id());
+            wx_recommend_earnings.setWeixin_id(userinfo.getWeixin_id());
+            int id = daoFactory.getWx_recommend_earningsDao().insert(wx_recommend_earnings);
+            if(id < 1){
+                returnData.setReturnData(errorcode_data, "添加申请成为合作人失败", "","2");
+                return;
+            }
+            returnData.setReturnData("0", "添加成功", wx_recommend_earnings);
+        } catch (Exception e) {
+            log.error("GoodsBiz ApplyUser is error", e);
+            returnData.setReturnData(errorcode_systerm, "GoodsBiz ApplyUser is error", "");
+        }
+
+        /**
+         * 获取合伙人的收益汇总
+         */
+    public void GainApplyUser(ReturnDataNew returnData, Param_userinfo param) {
+        try {
+            String sql = "select * from wx_recommend_earnings where ui_id=? and state=1 limit 1";
+            Wx_recommend_earnings wx_recommend_earnings = getDB().queryUniqueT(sql,Wx_recommend_earnings.class,param.getUi_id());
+            returnData.setReturnData("0", "获取合伙人的收益汇总成功", wx_recommend_earnings);
+        } catch (Exception e) {
+            log.error("GoodsBiz GainApplyUser is error", e);
+            returnData.setReturnData(errorcode_systerm, "GoodsBiz GainApplyUser is error", "");
+        }
+    }
+
     /****************************下面是封装的查询方法********************************/
     /**
      * 通过weixin_id  获取用户基本信息
