716280885b2d9a1ecea1ec31fe6e079289f19e28
jingxiaohu <251878350@qq.com>
2017-12-12 14:12:32 +0800

    修改用户表字段添加微信号码

--- src/main/java/com/pyb/mvc/action/v1/weixin/test2.java
+++ src/main/java/com/pyb/mvc/action/v1/weixin/test2.java
@@ -2,6 +2,9 @@ package com.pyb.mvc.action.v1.weixin;
 
 import com.alibaba.fastjson.JSONObject;
 import com.pyb.mvc.action.v1.BaseV1Controller;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class test2 extends BaseV1Controller {
@@ -34,8 +40,11 @@ public class test2 extends BaseV1Controller {
          Data	String	推送物流单号轨迹集合	R
          */
         try {
-            System.out.println(convertStreamToString(request.getInputStream()));
-        } catch (IOException e) {
+//            System.out.println(convertStreamToString(request.getInputStream()));
+            Map<String, String> map =  parseXml( request);
+            System.out.println("map="+map);
+        } catch (Exception e) {
+
             e.printStackTrace();
         }
 
@@ -88,7 +97,30 @@ public class test2 extends BaseV1Controller {
         return   baos.toString();
     }
 
+    public static Map<String, String> parseXml(HttpServletRequest request) throws Exception {
+        // 将解析结果存储在HashMap中
+        Map<String, String> map = new HashMap<String, String>();
 
+        // 从request中取得输入流
+        InputStream inputStream = request.getInputStream();
+        // 读取输入流
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(inputStream);
+        // 得到xml根元素
+        Element root = document.getRootElement();
+        // 得到根元素的所有子节点
+        List<Element> elementList = root.elements();
+
+        // 遍历所有子节点
+        for (Element e : elementList)
+            map.put(e.getName(), e.getText());
+
+        // 释放资源
+        inputStream.close();
+        inputStream = null;
+
+        return map;
+    }
 
 }
 
