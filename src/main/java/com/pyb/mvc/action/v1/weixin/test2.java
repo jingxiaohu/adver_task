f99388ef4e19de24c82dda013bc3d34ab2b6376a
jingxiaohu <251878350@qq.com>
2017-12-12 13:09:35 +0800

    修改用户表字段添加微信号码

--- src/main/java/com/pyb/mvc/action/v1/weixin/test2.java
+++ src/main/java/com/pyb/mvc/action/v1/weixin/test2.java
@@ -1,12 +1,17 @@
 package com.pyb.mvc.action.v1.weixin;
 
+import com.alibaba.fastjson.JSONObject;
 import com.pyb.mvc.action.v1.BaseV1Controller;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 public class test2 extends BaseV1Controller {
@@ -17,10 +22,30 @@ public class test2 extends BaseV1Controller {
         mav.addObject("message","hello kitty");
         return mav;
     }
-    @RequestMapping(value = "/kuaidi")
-    public void kuaidi(HttpServletRequest request, HttpServletResponse response) {
-        System.out.println(request.getQueryString());
-        sendRespHtml("SUCCESS",response);
+    @RequestMapping(value = "/kuaidi",method = RequestMethod.POST)
+    public void kuaidi(HttpServletRequest request, HttpServletResponse response, @Param("EBusinessID")String EBusinessID
+    ,String PushTime,String count,String Data
+    ) {
+        System.out.println("getQueryString="+request.getQueryString());
+        /**
+         * 应用级输入参数
+         参数名称	类型	说明	必须要求
+         EBusinessID	String	用户电商ID	R
+         PushTime	String	推送时间	R
+         Count	String	推送物流单号轨迹个数	R
+         Data	String	推送物流单号轨迹集合	R
+         */
+        System.out.println("EBusinessID="+EBusinessID+" PushTime="+PushTime+" count="+count);
+        System.out.println("Data="+Data);
+
+
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        JSONObject returnobj = new JSONObject();
+        returnobj.put("UpdateTime",sf.format(new Date()));
+        returnobj.put("Success",true);
+        returnobj.put("Reason","");
+
+        sendRespHtml(returnobj.toJSONString(),response);
     }
 }
 
