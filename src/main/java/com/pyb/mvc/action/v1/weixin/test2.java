7539c565ff563ec027333a7b48de4383adf5f4c1
jingxiaohu <251878350@qq.com>
2017-12-12 13:51:22 +0800

    修改用户表字段添加微信号码

--- src/main/java/com/pyb/mvc/action/v1/weixin/test2.java
+++ src/main/java/com/pyb/mvc/action/v1/weixin/test2.java
@@ -2,7 +2,6 @@ package com.pyb.mvc.action.v1.weixin;
 
 import com.alibaba.fastjson.JSONObject;
 import com.pyb.mvc.action.v1.BaseV1Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -23,8 +23,7 @@ public class test2 extends BaseV1Controller {
         return mav;
     }
     @RequestMapping(value = "/kuaidi",method = RequestMethod.POST)
-    public void kuaidi(HttpServletRequest request, HttpServletResponse response, @RequestBody JSONObject map
-    ) {
+    public void kuaidi(HttpServletRequest request, HttpServletResponse response) {
         System.out.println("getQueryString="+request.getQueryString());
         /**
          * 应用级输入参数
@@ -34,11 +33,11 @@ public class test2 extends BaseV1Controller {
          Count	String	推送物流单号轨迹个数	R
          Data	String	推送物流单号轨迹集合	R
          */
-//        System.out.println("EBusinessID="+EBusinessID+" PushTime="+PushTime+" count="+count);
-        if(map != null){
-            System.out.println("map="+map.toString());
+        try {
+            System.out.println(convertStreamToString(request.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
 
 
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -49,5 +48,47 @@ public class test2 extends BaseV1Controller {
 
         sendRespHtml(returnobj.toJSONString(),response);
     }
+
+    public String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "/n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public   String   inputStream2String2   (InputStream   in)   throws   IOException   {
+        StringBuffer   out   =   new   StringBuffer();
+        byte[]   b   =   new   byte[4096];
+        for   (int   n;   (n   =   in.read(b))   !=   -1;)   {
+            out.append(new   String(b,   0,   n));
+        }
+        return   out.toString();
+    }
+    public   static   String   inputStream2String3(InputStream   is)   throws IOException {
+        ByteArrayOutputStream baos   =   new   ByteArrayOutputStream();
+        int   i=-1;
+        while((i=is.read())!=-1){
+            baos.write(i);
+        }
+        return   baos.toString();
+    }
+
+
+
 }
 
