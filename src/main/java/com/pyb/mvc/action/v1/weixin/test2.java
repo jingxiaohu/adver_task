e416e854db012f0e1ea6d2ad68cb3cc7b16407f1
jingxiaohu <251878350@qq.com>
2017-12-12 13:30:19 +0800

    修改用户表字段添加微信号码

--- src/main/java/com/pyb/mvc/action/v1/weixin/test2.java
+++ src/main/java/com/pyb/mvc/action/v1/weixin/test2.java
@@ -2,7 +2,7 @@ package com.pyb.mvc.action.v1.weixin;
 
 import com.alibaba.fastjson.JSONObject;
 import com.pyb.mvc.action.v1.BaseV1Controller;
-import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
 @RestController
 public class test2 extends BaseV1Controller {
@@ -23,8 +24,7 @@ public class test2 extends BaseV1Controller {
         return mav;
     }
     @RequestMapping(value = "/kuaidi",method = RequestMethod.POST)
-    public void kuaidi(HttpServletRequest request, HttpServletResponse response, @Param("EBusinessID")String EBusinessID
-    ,String PushTime,String count,String Data
+    public void kuaidi(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String, Object> map
     ) {
         System.out.println("getQueryString="+request.getQueryString());
         /**
@@ -35,8 +35,11 @@ public class test2 extends BaseV1Controller {
          Count	String	推送物流单号轨迹个数	R
          Data	String	推送物流单号轨迹集合	R
          */
-        System.out.println("EBusinessID="+EBusinessID+" PushTime="+PushTime+" count="+count);
-        System.out.println("Data="+Data);
+//        System.out.println("EBusinessID="+EBusinessID+" PushTime="+PushTime+" count="+count);
+        if(map != null){
+            System.out.println("map="+map.toString());
+        }
+
 
 
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
